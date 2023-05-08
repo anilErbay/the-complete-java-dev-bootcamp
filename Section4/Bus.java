29ee86b8be3642c563957105b116864d74ac0d59
anilErbay <anilerbay@gmail.com>
2023-05-08 14:21:58 +0300

    check

---
+++ Section4/Bus.java
@@ -0,0 +1,11 @@
+public class Bus {
+    public static void main(String[] args) {
+        int passengers = 0;
+        passengers = 7;
+        passengers += 3;
+        System.out.println(passengers);
+
+        passengers -= 1;
+        System.out.println(passengers);
+    }
+}
---
+++ Section4/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int apples = 6;
+        long population = 8_000_000_000_000L;
+        double price = 5.99;
+        String name = "tommy";
+
+        int passengers = 5;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        int maxValue = 214748364;
+
+    }
+}
