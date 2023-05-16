ad36187b959192bedfe31b693c22c5346b75e9f8
anilErbay <anilerbay@gmail.com>
2023-05-16 19:56:14 +0300

    check

---
+++ Section12/src/ArrayListDemo.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> cities = new ArrayList<>();
+        List<String> citiesList = new ArrayList<>();
+    }
+}
